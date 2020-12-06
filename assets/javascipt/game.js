83c21ae395b97a9c3c625c179d05a818045135cc
Alexander <thezerothousand@gmail.com>
2020-12-06 15:20:54 -0600

    changed teams to objects

--- assets/javascipt/game.js
+++ assets/javascipt/game.js
@@ -1,144 +1,66 @@
 console.log("hi")
 
-// var shotCount1 = 0;
-// var teamOneNumshots = document.querySelector("#teamone-numshots");
-// var teamOneShootButton = document.getElementById("#teamone-shoot-button");
-//     teamOneShootButton.addEventListener("click", function() {
+const resetButton = document.getElementById('reset-button');
+const teamOneShootButton = document.getElementById('teamone-shoot-button');
+const teamTwoShootButton = document.getElementById('teamtwo-shoot-button');
+let numResets = document.getElementById('num-resets');
+let teamOneShotsDisplay = document.getElementById('teamone-numshots');
+let teamOneGoal = document.getElementById('teamone-numgoals')
+let teamTwoShotsDisplay = document.getElementById('teamtwo-numshots');
+let teamTwoGoal = document.getElementById('teamtwo-numgoals');
+let resetcounter = 0;
 
-//     let shotCount1 = Number(teamOneNumshots.innerHTML) + 1;
-//     teamOneNumshots.innerHTML = shotCount1;
-
-// })
-
-// TEAM 1
-// var button1 = document.getElementById("teamone-shoot-button");
-// var shots1 = document.getElementById("teamone-numshots");
-// count1 = 0;
-
-// button1.onclick = function () {
-//     count1 += 1;
-//     shots1.innerHTML = " " + count1;
-// }
-
-
-
-
-// // TEAM 2
-
-// var teamTwoShoot = document.getElementById("teamtwo-shoot-button");
-// var teamTwoShots = document.getElementById("teamtwo-numshots");
-// teamTwoGoal = document.getElementById("teamtwo-numgoals");
-// teamTwoGoal = 0;
-// teamTwoShotss = 0;
-
-// teamTwoShoot.onclick = function () {
-//     teamTwoShotss += 1;
-//     teamTwoShots.innerHTML = " " + teamTwoShotss;
-//     // let teamTwoGoal = Math.random(teamTwoGoal.innerHTML)
-//     var chanceHit2 = Math.ceil(Math.random() * 5)
-//     console.log(chanceHit2)
-//     if (chanceHit2 === 5) {
-//         teamTwoGoal += 1;
-//         teamTwoGoal.innerHTML = " " + teamTwoGoal;
-//         console.log(teamTwoGoal.innerHTML)
-
-//     }
-
-// }
-
-
-// TEAM 1
-
-let teamOneShootButton = document.getElementById("teamone-shoot-button");
-let teamOneGoal = document.getElementById("teamone-numgoals");
-let teamOneNumshots = document.getElementById("teamone-numshots")
-
-
-teamOneShootButton.addEventListener("click", function () {
-    teamOneShootUp = Number(teamOneNumshots.innerHTML) + 1;
-    teamOneNumshots.innerHTML = teamOneShootUp;
-    if (Math.random() * 100 < 40) {
-
-        teamOneGoalUp = Number(teamOneGoal.innerHTML) + 1;
-        teamOneGoal.innerHTML = teamOneGoalUp
-
-    //         var canvas = document.getElementById("myCanvas");
-    // var ctx = canvas.getContext("2d");
-    // var ballRadius = 10;
-    // var x = canvas.width/2;
-    // var y = canvas.height-30;
-    // var dx = 2;
-    // var dy = -2;
-
-    // function drawBall() {
-    //     ctx.beginPath();
-    //     ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-    //     ctx.fillStyle = "#0095DD";
-    //     ctx.fill();
-    //     ctx.closePath();
-    // }
-
-    // function draw() {
-    //     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    //     drawBall();
-        
-    //     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
-    //         dx = -dx;
-    //     }
-    //     if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
-    //         dy = -dy;
-    //     }
-        
-    //     x += dx;
-    //     y += dy;
+class Scoreboard {
+    constructor(shots, goals, team) {
+        this.shots = shots;
+        this.goals = goals;
+        this.score = 0;
+        this.team = team;
     }
-
-    // setInterval(draw, 10);
-    })
-
-
-// TEAM 2
-
-let teamTwoShootButton = document.getElementById("teamtwo-shoot-button");
-let teamTwoGoal = document.getElementById("teamtwo-numgoals");
-let teamTwoNumshots = document.getElementById("teamtwo-numshots")
-
-teamTwoShootButton.addEventListener("click", function () {
-    teamTwoShootUp = Number(teamTwoNumshots.innerHTML) + 1;
-    teamTwoNumshots.innerHTML = teamTwoShootUp;
-    if (Math.random() * 100 < 40) {
-
-        teamTwoGoalUp = Number(teamTwoGoal.innerHTML) + 1;
-        teamTwoGoal.innerHTML = teamTwoGoalUp
+    shoot() {
+        this.shots += 1;
+        this.team.innerHTML = this.shots
+        if (Math.random() * 100 < 30) {
+            this.score += 1;
+            this.goals.innerHTML = this.score;
+        }
     }
+    reset() {
+        this.shots = 0;
+        this.team.innerHTML = this.shots;
+        this.score = 0
+        this.goals.innerHTML = this.score;
+    }
+}
+
+let teamOne = new Scoreboard(0, teamOneGoal, teamOneShotsDisplay)
+let teamTwo = new Scoreboard(0, teamTwoGoal, teamTwoShotsDisplay)
+
+let reset = function (){
+    if (teamOne.score > teamTwo.score) {
+        alert('Team One Wins!');
+    } else if (teamOne.score < teamTwo.score) {
+        alert('Team Two Wins!')
+    }
+    if (teamOne.score === teamTwo.score) {
+        alert(`It's a draw!`)
+    }
+    resetcounter += 1
+    numResets.innerHTML = resetcounter
+}
+
+teamOneShootButton.addEventListener("click", function(){
+    teamOne.shoot()
+
 })
 
+teamTwoShootButton.addEventListener("click", function(){
+    teamTwo.shoot()
 
-// RESET COUNTER
-
-let resetButton = document.getElementById("reset-button");
-let resetNum = document.getElementById("num-resets");
-
-resetButton.addEventListener("click", function () {
-    document.getElementById("teamtwo-numshots").innerHTML = 0;
-    document.getElementById("teamtwo-numgoals").innerHTML = 0;
-    document.getElementById("teamone-numshots").innerHTML = 0;
-    document.getElementById("teamone-numgoals").innerHTML = 0;
-    resetNumAdd = Number(resetNum.innerHTML) + 1;
-    resetNum.innerHTML = resetNumAdd;
-
-    if(teamOneGoalUp > teamTwoGoalUp) {
-    alert("Team One WINS!!!");
-    } else {
-        alert("Team Two WINS!!!");
-    }
-    
 })
 
-
-
-
-
-
-
-
+resetButton.addEventListener("click", function (){
+    reset()
+    teamOne.reset();
+    teamTwo.reset();
+})
